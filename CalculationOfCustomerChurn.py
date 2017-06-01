533c7bc848c11c59aad6ffdef93083e2478315ce
PSurovenko1991 <grdg2010@mail.ru>
2017-06-02 01:58:36 +0600

    threeCommit

--- CalculationOfCustomerChurn.py
+++ CalculationOfCustomerChurn.py
@@ -6,67 +6,52 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.cross_validation import train_test_split
 from sklearn.naive_bayes import GaussianNB
-from collections import Counter
 
 
-data_set = pd.read_csv('https://raw.githubusercontent.com/chrisstroud/data/master/Churn_Modelling.csv')
+data_set = pd.read_csv('https://raw.githubusercontent.com/chrisstroud/data/master/Churn_Modelling.csv') #or attached file
+
 print(data_set.head())
-
-
 
 #Formalization:
 
-# geograph = set(data_set['Geography'])
-# print(geograph)
-# gende = set(data_set['Gender'])
-# print(gende)
-data_set['Geography'].replace('France',0,inplace= True)  # 
-data_set['Geography'].replace('Spain',1,inplace= True)
-data_set['Geography'].replace('Germany',2,inplace= True)
+for i in set(data_set["Geography"]):
+    data_set.insert(data_set.columns.size-1,i,data_set['Geography']== i)
+data_set = data_set.drop(["Geography"], axis=1)
+
 data_set['Gender'].replace('Male',0,inplace= True)   
 data_set['Gender'].replace('Female',1,inplace= True)
 
-# Correlation matrix:
+
+
+#Correlation matrix:
 
 correlation = data_set.corr()
 
 
-plt.figure(figsize=(15,15))
-sns.heatmap(correlation, vmax=0.5, square=True,  annot=True, cmap='cubehelix') 
+plt.figure(figsize=(25,25))
+sns.heatmap(correlation, vmax=1, square=True,  annot=True, cmap='cubehelix') 
 plt.show()
 
-
-# print(np.sort(correlation.Exited)>0)
-
-
-
-print (np.extract(abs((correlation.Exited))>0.11, (correlation.columns)))
-
-# print(corellation.columns)
-# print("!!!!!!!!!!!!!!!!!!!!!!!!!!")
-# print(correlation.Exited.flags())
-
-# print(correlation.Exited[np.extract(np.sort(correlation.Exited)>0, np.sort(correlation.Exited))])
-
-# print(correlation.Exited[np.extract(np.sort(correlation.Exited)>0, np.sort(correlation.Exited))])
-
-# print(data_set.shape)
-
-train = data_set[:8000]
-target = data_set[8001:]
-
-# print(train)
-# print("!!!!!!!!!!!!!!!!!!!!")
-# print(target)
-
-# for i in data_set.columns:
-#     print(i)
-#     print(i, (sum(sp.isnan(data_set['Surname']))))
-# print(sum(sp.isnan(data_set['Surname'])))/
+#Find important fields:
+fields =  (np.extract(abs((correlation.Exited))>0.12, (correlation.columns)))
+fields = fields[:-1]
+print("Important fields: ",fields)
 
 
-# X_train, X_test, y_train, y_test = train_test_split(train, target, test_size=0.25, random_state=0,)
 
-# clf = GaussianNB()
-# clf.fit(X_train ,y_train)
-# clf.score(X_test, y_test)
+#Formation of test sets:
+
+train = data_set[fields]
+target = data_set["Exited"]
+
+X_train, X_test, y_train, y_test = train_test_split(train, target, test_size=0.6, random_state=0,)
+
+#Training:
+clf = GaussianNB()
+clf.fit(X_train ,y_train)
+
+#Test:
+clf.score(X_test, y_test)
+
+
+
